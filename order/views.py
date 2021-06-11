c09b4b979ecf5ce3b27e2196f65370d2692f585b
antosha-chehonte <anton_cherkashin@list.ru>
2021-06-11 18:23:31 +0700

    удалить лишние пробелы, загрузка файла без таблицы

--- order/views.py
+++ order/views.py
@@ -1,32 +1,41 @@
+from bs4 import BeautifulSoup
 from datetime import date
 import mammoth
 from django.shortcuts import render
-from order.models import Order
 
 
 def order(request):
-
-    date_now = date.today()
+    today = date.today()
+    # output_filename = 'C:/py/sitedir/templates/include/order/result.html'
 
     if request.method == 'POST':
+
         input_docx_file = request.FILES['file'].file
         result = mammoth.convert_to_html(input_docx_file)
+        # text = result.value
+        text = ' '.join(result.value.split())
+        soup = BeautifulSoup(text, "html.parser")
+        cels = soup.find_all('td')
 
-        order_text = Order(text=result.value, date=date_now, number='номер приказа', title='название приказа')
-        order_text.save()
-        text = Order.objects.last()
-        count = Order.objects.all().count()
+        if cels:
+            cel1 = cels[0]
+            cel2 = cels[1]
+        else:
+            cel1 = "cel1 отсутствует"
+            cel2 = "cel2 отсутствует"
+
+        with open('C:/py/project/templates/include/order/result.html', 'w', encoding='utf-8') as text_file:
+            text_file.write(text)
 
         context = {
-            'text': text,
-            'count': count,
-                    }
-        return render(request, template_name="order/order_upload_success.html", context=context)
-    else:
-        text = Order.objects.last()
-        count = Order.objects.all().count()
-        context = {
-            'text': text,
-            'count': count,
+            'today': today,
+            'table': cels,
+            'cel1': cel1,
+            'cel2': cel2,
         }
-        return render(request, template_name="order/order_upload.html", context=context)
+        return render(request, template_name="order/order_upload_success.html", context=context)
+
+    context = {
+        'today': today,
+    }
+    return render(request, template_name="order/order_upload.html", context=context)
